529a5005b147d8c656a8baa66ed39aeec8507241
leo-ohasi <jlohasi@gmail.com>
2023-05-04 22:08:35 -0300

    feat: criação da page Focus Time

---
+++ src/home.js
@@ -0,0 +1,9 @@
+// criando variável do botão
+
+const buttonTime = document.getElementById("focusTime-button")
+buttonTime.addEventListener("click", pageFocusTime)
+
+function pageFocusTime(){
+    window.location.href = "../focusTime/index.html"
+}
+
