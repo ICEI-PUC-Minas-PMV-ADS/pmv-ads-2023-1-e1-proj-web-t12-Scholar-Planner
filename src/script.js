492ed902e5c9932c0e317b8111c26cc7a4acc335
Alice Coelho <dev.alicecoelho@gmail.com>
2023-05-06 19:24:56 -0300

    styles: ajustes de estilizações na pagina de login

--- src/script.js
+++ src/script.js
@@ -1,29 +1,12 @@
-// nossas variaveis / endereço de nossas memórias
 const inputUsername = document.getElementById('username');
 const loginButton = document.getElementById('login-button');
 
-// addEventLister
-// ouvir determinado evento
-// login.addEventListener('nomeDoEvento', nomeDaFuncao)
-
 loginButton.addEventListener('click', login);
-
-// condional estiver certa
-// entao faça tal coisa
-
-// se nao
-// faça tal coisa
-
-// ! negação
-
-// = atribuição
-// == comprando compra o dado
-// === comprando igual mesmo semelhante // tipo do dado, number, string
 
 function login() {
   if (!inputUsername.value) {
     alert('Digite o seu nome');
   } else {
-    window.location.href = 'home.html';
+    window.location.href = '../home/home.html';
   }
 }
