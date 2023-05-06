00f9109b03800853a1e28d4625c5ae52e4d6fa89
Alice Coelho <dev.alicecoelho@gmail.com>
2023-05-06 19:24:28 -0300

    feat: cria pasta para page  home

--- src/home.js
+++
@@ -1,9 +0,0 @@
-// criando variável do botão
-
-const buttonTime = document.getElementById("focusTime-button")
-buttonTime.addEventListener("click", pageFocusTime)
-
-function pageFocusTime(){
-    window.location.href = "../focusTime/index.html"
-}
-
---
+++ src/home/home.js
@@ -0,0 +1,6 @@
+const buttonTime = document.getElementById('focusTime-button');
+buttonTime.addEventListener('click', pageFocusTime);
+
+function pageFocusTime() {
+  window.location.href = '../focus-time/index.html';
+}
