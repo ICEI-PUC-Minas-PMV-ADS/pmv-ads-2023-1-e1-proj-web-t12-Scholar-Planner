ba81bfc5b7191b5136642e8d4edc69c91d033417
Alice Coelho <dev.alicecoelho@gmail.com>
2023-05-10 18:44:30 -0300

    fix: clean code

--- src/home/home.js
+++ src/home/home.js
@@ -11,12 +11,4 @@ function pageFocusTime() {
   window.location.href = '../focus-time/index.html';
 }
 
-const buttonteste = document.getElementById('teste');
-buttonteste.addEventListener('click', pagteste);
-
-function pagteste() {
-  console.log('batata');
-  window.location.href = '../testedraganddrop/index.html';
-}
-
 getNameAndInsertsItOnTheScreen();
