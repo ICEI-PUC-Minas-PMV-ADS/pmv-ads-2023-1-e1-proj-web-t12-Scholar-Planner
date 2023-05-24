0f92cd018468af73a8db0a26bfbb7e1cf33747eb
shigerysasaki <shigerysasaki12@hotmail.com>
2023-05-24 20:08:39 -0300

    fit: card

--- src/home/home.js
+++ src/home/home.js
@@ -31,14 +31,28 @@ function createColumn() {
   const column = document.createElement('div');
   column.classList.add('column');
   column.id = `column-${columnsArray.length}`;
+  column.innerHTML = `<div class="card-list"></div>`;
 
   const removeColumnButton = document.createElement('button');
+  removeColumnButton.classList.add("removeColumnButton")
   removeColumnButton.innerHTML = 'Excluir coluna';
   removeColumnButton.addEventListener('click', removeCurrentColumn);
+
+  const createCardButton = document.createElement('button');
+  createCardButton.innerHTML = 'ADD Cartão';
+  createCardButton.addEventListener('click', createCard);
+
+ 
+  column.appendChild(createCardButton);
+  
 
   column.appendChild(removeColumnButton);
   columnsContainer.appendChild(column);
   columnsArray.push(column);
+
+ 
+
+
 }
 
 addNewColumn.addEventListener('click', checkTheLengthOfArray);
@@ -47,4 +61,26 @@ function removeCurrentColumn(event) {
   let currentColumn = event.currentTarget.parentElement;
   columnsArray.splice(Number(currentColumn.id.split('-').at(-1)), 1);
   columnsContainer.removeChild(currentColumn);
+
 }
+
+function createCard(event){
+  let currentColumn = event.currentTarget.parentElement;
+  let conteudo =`<p class="name-task">nome da tarefa</p>
+  <div class="content-card">anotações</div>
+    <div class="box-actions-button">
+    <button class="save-button">V</button>
+    <div class="guia-drag">•••</div>
+    <button class="delete-button">X</button>
+  </div>`;
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.id = `card-${currentColumn.id}-0`;
+  card.innerHTML = conteudo; 
+  let cardList = currentColumn.children[0]; 
+  cardList.appendChild (card)
+}
+
+
+
+
