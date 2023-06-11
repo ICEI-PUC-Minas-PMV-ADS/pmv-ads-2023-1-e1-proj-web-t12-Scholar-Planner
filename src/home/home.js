a409f24a7415b1191b1d55aecef02cda22c2ad40
Lucasmarlon7 <lucasmarlon1212@gmail.com>
2023-06-11 15:13:53 -0300

    feat:atualizando a main

--- src/home/home.js
+++ src/home/home.js
@@ -1,3 +1,8 @@
+// Criei overlaymodal pra fazer o pop-up - ohasi
+// Atualizei eventlisteners e variáveis Todo
+// func renderTodos atualizada
+
+
 const buttonTime = document.getElementById('focusTime-button');
 const username = document.querySelector('.username');
 
@@ -13,38 +18,201 @@ function pageFocusTime() {
 
 getNameAndInsertsItOnTheScreen();
 
-// COLUNAS
 const columnsContainer = document.getElementById('columns-container');
 const addNewColumn = document.getElementById('add-column-button');
+const overlayModal = document.getElementById('overlay-content');
+const inputNameColumns = document.getElementById('name-column-input');
+const saveNamesColumn = document.querySelector('.save-column-name-button');
+const closeModalButton = document.querySelector('.modal-close');
 
-const columnsArray = [];
+let getColumns =
+  JSON.parse(localStorage.getItem('@SchoolPlaner/columns')) || [];
+
+function openModalCreateColumn() {
+  overlayModal.style.display = 'flex';
+}
 
 function checkTheLengthOfArray() {
-  if (columnsArray.length < 7) {
-    createColumn();
+  if (getColumns.length < 7) {
+    openModalCreateColumn();
   } else {
     alert('Número máximo de colunas atingido.');
   }
 }
 
-function createColumn() {
-  const column = document.createElement('div');
-  column.classList.add('column');
-  column.id = `column-${columnsArray.length}`;
+function createNewColumn() {
+  let columnValues;
 
-  const removeColumnButton = document.createElement('button');
-  removeColumnButton.innerHTML = 'Excluir coluna';
-  removeColumnButton.addEventListener('click', removeCurrentColumn);
+  if (inputNameColumns.value) {
+    columnValues = {
+      id: getColumns.length,
+      name: inputNameColumns.value,
+      tasks: [],
+    };
 
-  column.appendChild(removeColumnButton);
-  columnsContainer.appendChild(column);
-  columnsArray.push(column);
+    getColumns.push(columnValues);
+    saveColumn(getColumns);
+    overlayModal.style.display = 'none';
+    inputNameColumns.value = '';
+    renderColumn();
+  } else {
+    alert('Por favor, digite o nome da coluna');
+  }
 }
 
-addNewColumn.addEventListener('click', checkTheLengthOfArray);
+function renderColumn() {
+  columnsContainer.innerHTML = '';
 
-function removeCurrentColumn(event) {
-  let currentColumn = event.currentTarget.parentElement;
-  columnsArray.splice(Number(currentColumn.id.split('-').at(-1)), 1);
-  columnsContainer.removeChild(currentColumn);
+  getColumns.forEach((column) => {
+    const columnBox = document.createElement('div');
+    columnBox.classList.add('column');
+    columnBox.id = column.id;
+    const columnsName = document.createElement('p');
+    columnsName.classList.add("columnsname")
+    columnsName.innerHTML = column.name;
+
+    const removecolumn = document.createElement('button');
+    removecolumn.classList.add('removecolumn');
+    removecolumn.innerHTML = '<p id="excluircolunas"> EXCLUIR COLUNA</p>';
+    removecolumn.id = column.id;
+    removecolumn.addEventListener('click', delet);
+
+    const toDoListContainer = document.createElement('form');
+    toDoListContainer.classList.add('todo-form');
+    toDoListContainer.id = `todo-list-container-${column.id}`;
+    const todoListUl = document.createElement('ul');
+    todoListUl.id = `todo-list-${column.id}`;
+    const inputToNewTask = document.createElement('input');
+    inputToNewTask.id = `input-${column.id}`;
+    inputToNewTask.placeholder = 'Digite uma tarefa...';
+    const buttonToAddTask = document.createElement('button');
+    buttonToAddTask.innerText = '+';
+    buttonToAddTask.addEventListener('click', () => addTodoColumn(column.id));
+    toDoListContainer.append(inputToNewTask, buttonToAddTask);
+
+    columnBox.append(columnsName, toDoListContainer, todoListUl);
+    columnsContainer.appendChild(columnBox);
+    columnBox.appendChild(removecolumn);
+    renderTodos(column.id);
+  });
+}
+
+function delet(event) {
+  const buttonId = event.currentTarget.id;
+  const columnIndex = getColumns.findIndex((column) => column.id == buttonId);
+  if (columnIndex !== -1) {
+    getColumns.splice(columnIndex, 1);
+    saveColumn(getColumns);
+    renderColumn();
+  }
+}
+
+function closeModal() {
+  overlayModal.style.display = 'none';
+}
+
+renderColumn();
+
+addNewColumn.addEventListener('click', checkTheLengthOfArray);
+saveNamesColumn.addEventListener('click', createNewColumn);
+closeModalButton.addEventListener('click', closeModal);
+
+function saveColumn(column) {
+  localStorage.setItem('@SchoolPlaner/columns', JSON.stringify(column));
+}
+
+function renderTodos(id) {
+  const todoListUl = document.getElementById(`todo-list-${id}`);
+  todoListUl.innerHTML = '';
+
+  getColumns[id]?.tasks?.forEach((todo) => {
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
+
+    if (todo.done) {
+      li.classList.add('completed');
+    }
+
+    const cb = document.createElement('input');
+    cb.type = 'checkbox';
+    cb.checked = todo.done;
+    cb.addEventListener('change', () => toggleDone(todo.id, id));
+
+    li.appendChild(cb);
+
+    const span = document.createElement('span');
+    span.textContent = todo.text;
+
+    li.appendChild(span);
+
+    const xmlns = 'http://www.w3.org/2000/svg';
+
+    const btn = document.createElementNS(xmlns, 'svg');
+    btn.setAttribute('viewBox', '0 0 24 24');
+    btn.classList.add('remove-button');
+    btn.addEventListener('click', () => removeTodoColumn(todo.id, id));
+
+    const path = document.createElementNS(xmlns, 'path');
+    path.setAttribute(
+      'd',
+      'M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
+    );
+
+    btn.appendChild(path);
+
+    li.appendChild(btn);
+
+    todoListUl.appendChild(li);
+  });
+}
+
+// addToDoColumn atualizado - ohasi
+
+function addTodoColumn(id) {
+  const inputValue = document.getElementById(`input-${id}`);
+  const newTodoText = inputValue.value.trim();
+
+  if (!newTodoText) {
+    alert('Por favor, digite uma tarefa');
+  } else {
+    const newTodo = {
+      id: getColumns[id].tasks.length,
+      text: newTodoText,
+      done: false,
+    };
+    getColumns[id].tasks.push(newTodo);
+    saveColumn(getColumns);
+    renderTodos(id);
+    inputValue.value = '';
+  }
+}
+
+// função remove atualizada - ohasi
+
+function removeTodoColumn(todoId, columnId) {
+  const columnIndex = getColumns.findIndex((column) => column.id == columnId);
+  if (columnIndex !== -1) {
+    const todoIndex = getColumns[columnIndex].tasks.findIndex(
+      (todo) => todo.id === todoId
+    );
+    if (todoIndex !== -1) {
+      getColumns[columnIndex].tasks.splice(todoIndex, 1);
+      saveColumn(getColumns);
+      renderTodos(columnId);
+    }
+  }
+}
+
+// toggleDone atualizada - ohasi
+
+function toggleDone(todoId, columnId) {
+  const columnIndex = getColumns.findIndex((column) => column.id == columnId);
+  if (columnIndex !== -1) {
+    const todo = getColumns[columnIndex].tasks.find((todo) => todo.id === todoId);
+    if (todo) {
+      todo.done = !todo.done;
+      saveColumn(getColumns);
+      renderTodos(columnId);
+    }
+  }
 }
