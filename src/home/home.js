52660eb835ccc320f8af1f4656587043c02f252b
Marcos Kutova <kutova@gmail.com>
2023-06-15 21:26:48 -0300

    Update home.js

--- src/home/home.js
+++ src/home/home.js
@@ -2,92 +2,95 @@
 // Atualizei eventlisteners e variáveis Todo
 // func renderTodos atualizada
 
-
-const buttonTime = document.getElementById('focusTime-button');
-const username = document.querySelector('.username');
+const buttonTime = document.getElementById("focusTime-button");
+const username = document.querySelector(".username");
 
 function getNameAndInsertsItOnTheScreen() {
-  const usernameLocalStorage = localStorage.getItem('@SchoolPlaner/username');
+  const usernameLocalStorage = localStorage.getItem("@SchoolPlaner/username");
   username.innerHTML = usernameLocalStorage;
 }
 
-buttonTime.addEventListener('click', pageFocusTime);
+buttonTime.addEventListener("click", pageFocusTime);
 function pageFocusTime() {
-  window.location.href = '../focus-time/index.html';
+  window.location.href = "../focus-time/index.html";
 }
 
 getNameAndInsertsItOnTheScreen();
 
-const columnsContainer = document.getElementById('columns-container');
-const addNewColumn = document.getElementById('add-column-button');
-const overlayModal = document.getElementById('overlay-content');
-const inputNameColumns = document.getElementById('name-column-input');
-const saveNamesColumn = document.querySelector('.save-column-name-button');
-const closeModalButton = document.querySelector('.modal-close');
+const columnsContainer = document.getElementById("columns-container");
+const addNewColumn = document.getElementById("add-column-button");
+const overlayModal = document.getElementById("overlay-content");
+const inputNameColumns = document.getElementById("name-column-input");
+const saveNamesColumn = document.querySelector(".save-column-name-button");
+const closeModalButton = document.querySelector(".modal-close");
 
 let getColumns =
-  JSON.parse(localStorage.getItem('@SchoolPlaner/columns')) || [];
+  JSON.parse(localStorage.getItem("@SchoolPlaner/columns")) || [];
 
 function openModalCreateColumn() {
-  overlayModal.style.display = 'flex';
+  overlayModal.style.display = "flex";
 }
 
 function checkTheLengthOfArray() {
   if (getColumns.length < 7) {
     openModalCreateColumn();
   } else {
-    alert('Número máximo de colunas atingido.');
+    alert("Número máximo de colunas atingido.");
   }
 }
 
 function createNewColumn() {
   let columnValues;
+  let lastID = parseInt(localStorage.getItem("@SchoolPlaner/lastid")) || 0;
+  let nextID = lastID + 1;
 
   if (inputNameColumns.value) {
     columnValues = {
-      id: getColumns.length,
+      id: nextID,
       name: inputNameColumns.value,
       tasks: [],
     };
 
+    localStorage.setItem("@SchoolPlaner/lastid", nextID);
     getColumns.push(columnValues);
     saveColumn(getColumns);
-    overlayModal.style.display = 'none';
-    inputNameColumns.value = '';
+    overlayModal.style.display = "none";
+    inputNameColumns.value = "";
     renderColumn();
   } else {
-    alert('Por favor, digite o nome da coluna');
+    alert("Por favor, digite o nome da coluna");
   }
 }
 
 function renderColumn() {
-  columnsContainer.innerHTML = '';
+  columnsContainer.innerHTML = "";
 
   getColumns.forEach((column) => {
-    const columnBox = document.createElement('div');
-    columnBox.classList.add('column');
+    const columnBox = document.createElement("div");
+    columnBox.classList.add("column");
     columnBox.id = column.id;
-    const columnsName = document.createElement('p');
-    columnsName.classList.add("columnsname")
+    const columnsName = document.createElement("p");
+    columnsName.classList.add("columnsname");
     columnsName.innerHTML = column.name;
 
-    const removecolumn = document.createElement('button');
-    removecolumn.classList.add('removecolumn');
-    removecolumn.innerHTML = '<p id="excluircolunas"> EXCLUIR COLUNA</p>';
+    const removecolumn = document.createElement("button");
+    removecolumn.classList.add("removecolumn");
+    removecolumn.innerHTML =
+      '<img class="trash-icon" src="/images/trash-icon.png"><p id="excluircolunas">EXCLUIR COLUNA</p>';
     removecolumn.id = column.id;
-    removecolumn.addEventListener('click', delet);
+    removecolumn.addEventListener("click", delet);
 
-    const toDoListContainer = document.createElement('form');
-    toDoListContainer.classList.add('todo-form');
+    const toDoListContainer = document.createElement("form");
+    toDoListContainer.classList.add("todo-form");
     toDoListContainer.id = `todo-list-container-${column.id}`;
-    const todoListUl = document.createElement('ul');
+    const todoListUl = document.createElement("ul");
     todoListUl.id = `todo-list-${column.id}`;
-    const inputToNewTask = document.createElement('input');
+    const inputToNewTask = document.createElement("input");
     inputToNewTask.id = `input-${column.id}`;
-    inputToNewTask.placeholder = 'Digite uma tarefa...';
-    const buttonToAddTask = document.createElement('button');
-    buttonToAddTask.innerText = '+';
-    buttonToAddTask.addEventListener('click', () => addTodoColumn(column.id));
+    inputToNewTask.placeholder = "Digite uma tarefa...";
+    const buttonToAddTask = document.createElement("button");
+    buttonToAddTask.innerText = "+";
+    buttonToAddTask.addEventListener("click", () => addTodoColumn(column.id));
     toDoListContainer.append(inputToNewTask, buttonToAddTask);
 
     columnBox.append(columnsName, toDoListContainer, todoListUl);
@@ -108,54 +111,55 @@ function delet(event) {
 }
 
 function closeModal() {
-  overlayModal.style.display = 'none';
+  overlayModal.style.display = "none";
 }
 
 renderColumn();
 
-addNewColumn.addEventListener('click', checkTheLengthOfArray);
-saveNamesColumn.addEventListener('click', createNewColumn);
-closeModalButton.addEventListener('click', closeModal);
+addNewColumn.addEventListener("click", checkTheLengthOfArray);
+saveNamesColumn.addEventListener("click", createNewColumn);
+closeModalButton.addEventListener("click", closeModal);
 
 function saveColumn(column) {
-  localStorage.setItem('@SchoolPlaner/columns', JSON.stringify(column));
+  localStorage.setItem("@SchoolPlaner/columns", JSON.stringify(column));
 }
 
 function renderTodos(id) {
   const todoListUl = document.getElementById(`todo-list-${id}`);
-  todoListUl.innerHTML = '';
+  todoListUl.innerHTML = "";
+  const indice = getColumns.findIndex((x) => x.id == id);
 
-  getColumns[id]?.tasks?.forEach((todo) => {
-    const li = document.createElement('li');
-    li.classList.add('todo-item');
+  getColumns[indice]?.tasks?.forEach((todo) => {
+    const li = document.createElement("li");
+    li.classList.add("todo-item");
 
     if (todo.done) {
-      li.classList.add('completed');
+      li.classList.add("completed");
     }
 
-    const cb = document.createElement('input');
-    cb.type = 'checkbox';
+    const cb = document.createElement("input");
+    cb.type = "checkbox";
     cb.checked = todo.done;
-    cb.addEventListener('change', () => toggleDone(todo.id, id));
+    cb.addEventListener("change", () => toggleDone(todo.id, id));
 
     li.appendChild(cb);
 
-    const span = document.createElement('span');
+    const span = document.createElement("span");
     span.textContent = todo.text;
 
     li.appendChild(span);
 
-    const xmlns = 'http://www.w3.org/2000/svg';
+    const xmlns = "http://www.w3.org/2000/svg";
 
-    const btn = document.createElementNS(xmlns, 'svg');
-    btn.setAttribute('viewBox', '0 0 24 24');
-    btn.classList.add('remove-button');
-    btn.addEventListener('click', () => removeTodoColumn(todo.id, id));
+    const btn = document.createElementNS(xmlns, "svg");
+    btn.setAttribute("viewBox", "0 0 24 24");
+    btn.classList.add("remove-button");
+    btn.addEventListener("click", () => removeTodoColumn(todo.id, id));
 
-    const path = document.createElementNS(xmlns, 'path');
+    const path = document.createElementNS(xmlns, "path");
     path.setAttribute(
-      'd',
-      'M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
+      "d",
+      "M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
     );
 
     btn.appendChild(path);
@@ -171,19 +175,20 @@ function renderTodos(id) {
 function addTodoColumn(id) {
   const inputValue = document.getElementById(`input-${id}`);
   const newTodoText = inputValue.value.trim();
+  const indice = getColumns.findIndex((x) => x.id == id);
 
   if (!newTodoText) {
-    alert('Por favor, digite uma tarefa');
+    alert("Por favor, digite uma tarefa");
   } else {
     const newTodo = {
-      id: getColumns[id].tasks.length,
+      id: getColumns[indice].tasks.length,
       text: newTodoText,
       done: false,
     };
-    getColumns[id].tasks.push(newTodo);
+    getColumns[indice].tasks.push(newTodo);
     saveColumn(getColumns);
     renderTodos(id);
-    inputValue.value = '';
+    inputValue.value = "";
   }
 }
 
@@ -208,7 +213,9 @@ function removeTodoColumn(todoId, columnId) {
 function toggleDone(todoId, columnId) {
   const columnIndex = getColumns.findIndex((column) => column.id == columnId);
   if (columnIndex !== -1) {
-    const todo = getColumns[columnIndex].tasks.find((todo) => todo.id === todoId);
+    const todo = getColumns[columnIndex].tasks.find(
+      (todo) => todo.id === todoId
+    );
     if (todo) {
       todo.done = !todo.done;
       saveColumn(getColumns);
