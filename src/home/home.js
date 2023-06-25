b4fdc0fa065d47175ec7fa04e2208d40d526f000
Renan Stankevicius <renan.stankevicius@gmail.com>
2023-06-25 18:22:00 -0300

    fix: corrige scrollbar

--- src/home/home.js
+++ src/home/home.js
@@ -223,9 +223,3 @@ function toggleDone(todoId, columnId) {
   }
 }
 
-var item = document.getElementById("columns-container");
-
-  window.addEventListener("wheel", function (e) {
-    if (e.deltaY > 0) item.scrollLeft += 100;
-    else item.scrollLeft -= 100;
-  });
